ca5ee6dcfbc6fb75d564510134bc50d8d1f18f61
shlegel <sveta.shlegel@mail.ru>
2023-11-22 21:05:37 +0300

    sorted engaged employees with active tasks

--- employees/views.py
+++ employees/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
-from django.db.models import Count
+from django.db.models import Prefetch, Case, When, IntegerField, Sum
 
 from employees.models import Employee
 from employees.serializers import EmployeeSerializer
+from tasks.models import Task
 
 
 class EmployeeCreateAPIView(generics.CreateAPIView):
@@ -30,4 +31,6 @@ class EmployeeDestroyAPIView(generics.DestroyAPIView):
 
 class EngagedEmployeesListAPIView(generics.ListAPIView):
     serializer_class = EmployeeSerializer
-    queryset = Employee.objects.filter(task__isnull=False).annotate(task_count=Count('task')).order_by('-task_count')
+    queryset = (Employee.objects.filter(task__isnull=False).annotate(task_count=Sum(Case(When(task__is_completed=False,
+                then=1), default=0, output_field=IntegerField()))).order_by('-task_count').prefetch_related(Prefetch(
+        'task_set', queryset=Task.objects.filter(is_completed=False))))
