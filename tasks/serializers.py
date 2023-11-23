df43be302c56f99612979f43b65e0df2ccad5d5f
shlegel <sveta.shlegel@mail.ru>
2023-11-23 12:05:16 +0300

    added important tasks

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from tasks.models import Task
+from tasks.services import get_suitable_employee
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -8,3 +9,20 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class ImportantTaskSerializer(serializers.ModelSerializer):
+    available_employee = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Task
+        fields = ['id', 'deadline', 'available_employee']
+
+    def get_available_employee(self, obj):
+        """Реализует поиск сотрудников джля выполнения важных задач"""
+        available_emp = []
+        base_task_executor = obj.base_task.executor
+        employee = get_suitable_employee(base_task_executor)
+        res = f'{employee.first_name} {employee.second_name} - {employee.position}'
+        available_emp.append(res)
+        return available_emp
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from tasks.views import TaskCreateAPIView, TaskListAPIView, TaskRetrieveAPIView, TaskUpdateAPIView, TaskDestroyAPIView
+from tasks.views import (TaskCreateAPIView, TaskListAPIView, TaskRetrieveAPIView, TaskUpdateAPIView, TaskDestroyAPIView,
+                         ImportantTaskListAPIView)
 
 from tasks.apps import TasksConfig
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("detail/<int:pk>/", TaskRetrieveAPIView.as_view(), name='task_detail'),
     path("update/<int:pk>/", TaskUpdateAPIView.as_view(), name='task_update'),
     path("delete/<int:pk>/", TaskDestroyAPIView.as_view(), name='task_delete'),
+    path("important/", ImportantTaskListAPIView.as_view(), name='important_tasks'),
 ]
